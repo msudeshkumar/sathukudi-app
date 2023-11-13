ffe36166458fd3e3c94fd84e92502b83e0a557f5
Sudesh Kumar M <sudhesh@bytize.in>
2023-11-13 12:58:17 +0530

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,60 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
 export default function Home() {
+  const [countDown, setCountDown] = useState(null)
+
+  var countDownDate = new Date("Dec 3, 2023 00:00:00").getTime();
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    /* document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s "; */
+    setCountDown(days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s " + " to go...")
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      // document.getElementById("demo").innerHTML = "EXPIRED";
+      // setCountDown
+    }
+  }, 1000);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ backgroundColor: '#f8adc3', padding: 0 }}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/animation-animated.gif" />
       </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main style={{ width: '100%', display: 'flex' }}>
+          <div style={{ flex: 1, fontStyle: 'oblique', fontSize: 20 }}>
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+              <span style={{ color: 'salmon' }}>{countDown}</span>
+              <span style={{ marginBottom: 5 }}>
+                <img src="/cake-three.gif" alt="Vercel" style={{ height: 50 }} />
+              </span>
+            </div>
+          </div>
+          <div style={{ flex: 1 }}>
+            <img src="/ranjita.gif" alt="Vercel" style={{ flex: 1, maxWidth: '100%' }} />
+          </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
 
       <style jsx>{`
         main {
